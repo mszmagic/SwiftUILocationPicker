7c50a16d92402ce86b2f5000db458c75331ccebd
Shunzhe <git@mszmagic.com>
2021-01-20 00:26:12 -0500

    Fixing localization

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LocationPicker",
-            dependencies: ["SwiftUILibrary"],
-            resources: [.process("Resources")]),
+            dependencies: ["SwiftUILibrary"]),
     ]
 )
